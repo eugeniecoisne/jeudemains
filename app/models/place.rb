abe16d32af65eef5b89211ac499f5e726be096bd
eugeniecoisne <57906639+eugeniecoisne@users.noreply.github.com>
2020-02-24 18:12:31 +0100

    Update place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ApplicationRecord
-  belongs_to :profil
+  belongs_to :profile
   has_many :workshops
 
   validates :name, :address, :zip_code, :city, :phone_number, presence: true, allow_blank: false
