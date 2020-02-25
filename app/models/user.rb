ca9bb344f24037d7f5cbedba71cdeaad3a6f8230
eugeniecoisne <57906639+eugeniecoisne@users.noreply.github.com>
2020-02-25 14:41:10 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :profile, dependent: :destroy
+  has_one :profile
   after_create :create_profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
