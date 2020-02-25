acb3f473ff72be9c2403027dfbbe973e0f738979
eugeniecoisne <57906639+eugeniecoisne@users.noreply.github.com>
2020-02-25 14:39:08 +0100

    Update profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many :places, dependent: :destroy
-  has_many :reviews, dependent: :destroy
+  has_many :places
+  has_many :reviews
 
 end
