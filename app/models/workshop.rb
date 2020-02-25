dd7778f619f7e7ac4f28c01ebc25d89c013f097f
Riwan Talmo <riwan.talmo@gmail.com>
2020-02-25 12:34:16 +0100

    seed

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -4,10 +4,10 @@ class Workshop < ApplicationRecord
   has_many :reviews
   has_many :workshop_dates, dependent: :destroy
 
-  validates :level, inclusion: { in: ['Débutant', 'Intermédiaire', 'Avancé'] }
-  validates :status, inclusion: { in: ['En ligne', 'Hors ligne'] }
+  # validates :level, inclusion: { in: ['Débutant', 'Intermédiaire', 'Avancé'] }
+  # validates :status, inclusion: { in: ['En ligne', 'Hors ligne'] }
 
   validates :name, presence: true, allow_blank: false
 
-  validates :price, :duration, :participants, numericality: true
+  # validates :price, :duration, :participants, numericality: true
 end
