b67b95f3ababaa1917da559267f8422a271e5e96
Riwan Talmo <riwan.talmo@gmail.com>
2020-02-26 21:43:50 +0100

    search profile and place

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @profiles = Profile.where('organizer=true OR animator=true')
+    @profiles = Profile.where(animator: true)
+    @places = Place.joins(:profile).where("profiles.organizer=true")
   end
 
   def search
-
+    raise
   end
 end
