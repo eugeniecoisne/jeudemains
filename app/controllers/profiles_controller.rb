7066215c91c25fe960add461e4b0e9f4eae5ecf6
Riwan Talmo <riwan.talmo@gmail.com>
2020-02-27 15:16:16 +0100

    Add skip devise

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:public]
 
   def show
     authorize @profile
