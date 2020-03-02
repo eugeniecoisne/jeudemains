70e993841160f7943679ac7abe9534662bad3580
Riwan Talmo <riwan.talmo@gmail.com>
2020-03-02 20:31:37 +0100

    correct JS

--- app/javascript/plugins/show_more_reviews.js
+++ app/javascript/plugins/show_more_reviews.js
@@ -1,16 +1,17 @@
 const showMoreReviews = () => {
-  console.log('hello')
   const showMore = document.querySelector("#moreReview");
 
-  showMore.addEventListener("click", (event) => {
-    document.querySelector('.card-reviews').classList.toggle('show')
+  if (showMore) {
+    showMore.addEventListener("click", (event) => {
+      document.querySelector('.card-reviews').classList.toggle('show')
 
-    if (document.querySelector('.card-reviews').classList.contains('show')) {
-      event.currentTarget.innerText = 'Afficher moins de commentaires'
-    } else {
-      event.currentTarget.innerText = 'Afficher plus de commentaires'
-    }
-  });
+      if (document.querySelector('.card-reviews').classList.contains('show')) {
+        event.currentTarget.innerText = 'Afficher moins de commentaires'
+      } else {
+        event.currentTarget.innerText = 'Afficher plus de commentaires'
+      }
+    });
+  }
 }
 
 export { showMoreReviews };
