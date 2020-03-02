108ef2497dd1ea4b529f4f19f21a5c584d65a859
Riwan Talmo <riwan.talmo@gmail.com>
2020-03-02 14:51:50 +0100

    change status booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
-  mount StripeEvent::Engine, at: 'https://jeudemains.herokuapp.com/stripe-webhooks'
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
 
   root to: 'pages#home'
 
